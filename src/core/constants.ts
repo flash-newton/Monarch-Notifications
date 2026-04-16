f78a56f921324e531100099d59338150c5abdb23
Bimal Rajapakse <bimal.rajapakse@monarch360.com.au>
2026-04-16 15:42:45 +0530

    Updated list name

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,2 +1,2 @@
 /** Display name of the SharePoint list this library writes to */
-export const MONARCH_LIST_NAME = "Monarch Notifications";
+export const MONARCH_LIST_NAME = "MonarchNotifications";
